fead450b2c94b26ceae00825bd83f7a765128650
RidgeDev <studentjester42@gmail.com>
2013-12-04 09:09:18 +0000

    Tidied up Sketch.java

--- src/main/java/chrisRidgers/app/Sketch.java
+++ src/main/java/chrisRidgers/app/Sketch.java
@@ -14,19 +14,21 @@ public class Sketch extends PApplet
 
   public void setup()
   {              
-    ellipseMode(CENTER);
-    track = this.args[0];
-    minim = new Minim(this);
-    player = minim.loadFile(track, 2048);
+    track 	= this.args[0];
+    minim 	= new Minim(this);
+    player 	= minim.loadFile(track, 2048);
+    fft 	= new FFT(player.bufferSize(), player.sampleRate());
+
     player.loop();
-    fft = new FFT(player.bufferSize(), player.sampleRate());
     fft.window(FFT.HAMMING);
     fft.logAverages(20,20);
-    frameRate(30);
+
     size(400,400);
-    noFill();
-    stroke(255);
+    frameRate(30);
     background(0);
+    stroke(255);
+    noFill();
+    ellipseMode(CENTER);
     rectMode(CORNERS);
 
     if(frame!=null)
@@ -40,26 +42,28 @@ public class Sketch extends PApplet
     background(0);
     stroke(128);
     noFill();
+
     fft.forward(player.mix);
     float w = width/(float)fft.avgSize();
+
     for(int i=0;i<fft.avgSize();i++)
     {
       rect(i*w, height, i*w+w, height/1-Math.round(2*20*Math.log10(100*fft.getAvg(i))));
-      if(fft.getAvg(i)>260)System.out.println(fft.getAvg(i));
+      //if(fft.getAvg(i)>260)System.out.println(fft.getAvg(i));
       if(fft.getAvg(i)>1)ripples.add(new Ripple(this, i, fft.getAvg(i)));
     }
-    for(int i=0;i<ripples.size()-1;i++){
+
+    for(int i=0;i<ripples.size()-1;i++)
+    {
       Ripple test = (Ripple) ripples.get(i);
       test.display();
       test.move();
+
       if(test.o<=0)
       {
 	ripples.remove(i);
 	ripples.trimToSize();
       }
     }
-
-    //System.out.println(w);
-    //System.out.println(fft.avgSize());
   }
 }
