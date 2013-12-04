f15c3bf98f07221d8da0cc1dff2ee47383a34fe3
RidgeDev <studentjester42@gmail.com>
2013-12-04 04:24:00 +0000

    Still Major Devestation :'(

---
+++ soundVisApp/src/main/java/soundVisApp/App.java
@@ -0,0 +1,10 @@
+package chrisRidgers.app;
+import processing.core.*;
+
+public class App
+{
+  public static void main( String args[] )
+  {
+    PApplet.main(new String[] {/*"--present",*/ "chrisRidgers.app.Sketch", args[0] });
+  }
+}
